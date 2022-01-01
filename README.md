f39f2a72e5b3efc5468bece7b67c652169d7c0ac
movwf <movwf@protonmail.com>
2022-01-02 00:21:17 +0300

    Feat: Visualizing the 'App'

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hex Parser
+
+![Hex Parser](/img/hex-parser.png)
+
+> This is experimental script for constructing real files with only HEX strings.
+
+> It has interesting use cases 😈 :smiling_imp:
--- parser.js
+++ parser.js
@@ -1,11 +1,34 @@
 const fs = require("fs");
 
+/**
+ * Hex Parser
+ * Parsing hex strings filled file into file itself.
+ * @param {string} inFile Input File w/Extension (e.g in.txt)
+ * @param {string} outFile Output File w/Extension (e.g out.txt)
+ */
 const parser = (inFile, outFile) => {
+  console.log(`
+===============================================
+|       _         _          _    __   _   _  |
+| |_|  |_  \\/    |_)   /\\   |_)  (_   |_  |_) |
+| | |  |_  /\\    |    /--\\  | \\  __)  |_  | \\ |
+|                                             |
+| version 0.1.0                               |
+|                                             |
+|                                github/movwf |
+===============================================`);
+
+  console.log(`| File is parsing : ${inFile}`);
   fs.readFile(inFile, { encoding: "utf-8" }, (err, file) => {
     if (!err) {
       const buffer = Buffer.from(file, "hex");
 
-      fs.writeFileSync(outFile, buffer);
+      fs.writeFile(outFile, buffer, (err) => {
+        if (!err) {
+          console.log(`| File has created : ${outFile}`);
+          console.log("===============================================");
+        }
+      });
     }
   });
 };
